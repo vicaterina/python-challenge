270fd53a565fef8543030cffe11122a9b57e873f
vicaterina <vic.caterina@gmail.com>
2019-09-04 00:00:22 -0500

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,10 +7,23 @@ data=pd.read_csv("~/GitHub/python-challenge/PyPoll/election_data.csv")
 
 #Create a dataframe 
 df=pd.DataFrame(data)
+df.head()
+df[""]
+
+#Format header
+print("Election Results")
+print("__________________________________")
+print("\n")
 
 #Calculate the toal number of votes cast
+total_votes=len(df)
+print("Total Votes Cast: " +str(total_votes))
+print("__________________________________")
 
 #A complete list of candidates who received votes
+#Group by candidate
+#candidate_votes=df["Candidate"].value_counts()
+print(candidate_votes)
 
 #Total number of votes each candidate won
 
